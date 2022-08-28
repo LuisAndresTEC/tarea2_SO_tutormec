7da675c07863faa05d292e983f7ac93a5e575495
LuisAndresTEC <lrojasmurillo7@estudiantec.cr>
2022-08-27 21:26:00 -0600

    reparación commit anterior

--- boot.asm
+++ boot.asm
@@ -1,23 +1,20 @@
-org 0x7C00                      ; BIOS loads our programm at this address
-%include "secondary.asm"; Library made by my own, to simplify code
+org 0x7C00                      
+%include "secondary.asm"
 
 start:
     jmp main
-    init_message: db "    tutormek   ",10,0X0D\,
-    
-    second_message: db " _    _     _     _    _   ", 10,0X0D\,
+    init_message:   db "    tutormek   ", 10,0X0D\,
+                    db "a", 10,0X0D\,
+                    db " _    _     _     _    _   ", 10,0X0D\,
                     db "| |  | |   | |   | |  | |  ", 10,0X0D\,
                     db "| |  | |   | |   | |  | |  ", 10,0X0D\,
                     db "Para precionar la tecla utilica su dedo meñique ", 0
 
-    buffer: times 10 db 0,0;Buffer to save chars
-    none: db "",0
+    buffer: times 10 db 0,0
+    ;none: db "",0
 
 main: 
-    ;Prints e inputus del texto, posiciona el cursor para empezar la impresion
     print init_message
-    ;replacer
-    ;print second_message
     print_new_line
     input buffer
     mov bl,0
